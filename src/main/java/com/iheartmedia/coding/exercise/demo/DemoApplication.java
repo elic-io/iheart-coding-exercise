2096d8a113a96f5147ae57cdedbdba8a5a78863e
Jon Roberts <jon.roberts@agilecraft.com>
2017-11-29 15:36:41 -0600

    swagger springbox running

--- src/main/java/com/iheartmedia/coding/exercise/demo/DemoApplication.java
+++ src/main/java/com/iheartmedia/coding/exercise/demo/DemoApplication.java
@@ -1,12 +1,43 @@
 package com.iheartmedia.coding.exercise.demo;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.ComponentScan;
+import com.iheartmedia.coding.exercise.demo.controller.AdvertiserController;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.Tag;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.service.ApiKey;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.schema.WildcardType;
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.AuthorizationScope;
+
+
+import static com.google.common.collect.Lists.*;
+import static springfox.documentation.schema.AlternateTypeRules.*;
+*/
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
+	}	  
 }
